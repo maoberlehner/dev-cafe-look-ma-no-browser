e50709aebb849e716f54b6b8f2417f3017bba700
Markus Oberlehner <markus.oberlehner@gmail.com>
2021-11-20 08:29:49 +0100

    Update dependencies and enable Cypress tests

--- test/cypress/plugins/index.ts
+++ test/cypress/plugins/index.ts
@@ -1,24 +1,6 @@
-import { startDevServer } from '@cypress/vite-dev-server';
-import path from 'path';
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
-
 const plugin: Cypress.PluginConfig = (on, config) => {
   // eslint-disable-next-line global-require
   require(`cypress-watch-and-reload/plugins`)(config);
-
-  let bundler = createBundler({
-    define: {
-      global: `window`,
-    },
-  });
-  on(`file:preprocessor`, bundler);
-
-  on(`dev-server:start`, options => startDevServer({
-    options,
-    viteConfig: {
-      configFile: path.resolve(__dirname, `..`, `..`, `vite.config.ts`),
-    },
-  }));
 
   return config;
 };
